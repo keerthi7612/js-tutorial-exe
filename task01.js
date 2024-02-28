999125404e96c80730922f915b0e65b2b1bf321c
keerthi7612 <144551341+keerthi7612@users.noreply.github.com>
2024-02-28 19:56:26 +0530

    Add files via upload

---
+++ task01.js
@@ -0,0 +1,31 @@
+// Our array of values
+/*
+
+// TASK -1
+const myArray = ["T", "H", "R", "E", "E"];
+
+const countDuplicates = (arr) => {
+  const counts = {};
+
+  arr.forEach((value) => {
+    if (!counts[value]) {
+      counts[value] = 1;
+    } else {
+      counts[value]++;
+    }
+  });
+  return counts;
+};
+const result = countDuplicates(myArray);
+
+console.log(result);
+*/
+// TASK -2
+for (let i = 0; i <= 8; i++) {
+  for (let j = 1; j < 9; j++) {
+    for (let k = 6; k <= 8; k++) {
+      console.log("hii");
+    }
+  }
+}
+// NO.OF THE ITERATION IS 216 times
